577939250d45e1b591eb6d5d1564e4fa9128489b
AnhLeDuc7421 <renyk97@gmail.com>
2024-03-18 16:40:53 +0700

    fix

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,17 +1,27 @@
 import { Box } from '@mui/material'
-import ModeSelect from '../ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { FaTrello } from 'react-icons/fa'
 
 
 function AppBar() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.light',
       height:(theme) => theme.trello.appBarHeight,
       width:'100%',
       display:'flex',
-      alignItems:'center'
+      alignItems:'center',
+      justifyContent:'space-between'
     }}>
-      <ModeSelect></ModeSelect>
+      <Box>
+        <AppsIcon sx={{
+          color:'primary.main'
+        }}/>
+        <FaTrello size={24} color='primary.main'/>
+      </Box>
+      <Box>
+        <ModeSelect></ModeSelect>
+      </Box>
     </Box>
   )
 }
