16ae4ae2540e0b77ca9677c833e175743e3aa794
AnhLeDuc7421 <renyk97@gmail.com>
2024-03-19 11:06:28 +0700

    build app_bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,26 +1,88 @@
-import { Box } from '@mui/material'
+import { Badge, Box, TextField, Tooltip, Typography } from '@mui/material'
 import ModeSelect from '~/components/ModeSelect'
 import AppsIcon from '@mui/icons-material/Apps'
 import { FaTrello } from 'react-icons/fa'
-
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
+import Started from './Menus/Started'
+import Templates from './Menus/Templates'
+import Button from '@mui/material/Button'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import Profile from './Menus/Profile'
 
 function AppBar() {
   return (
-    <Box sx={{
-      height:(theme) => theme.trello.appBarHeight,
-      width:'100%',
-      display:'flex',
-      alignItems:'center',
-      justifyContent:'space-between'
-    }}>
-      <Box>
-        <AppsIcon sx={{
-          color:'primary.main'
-        }}/>
-        <FaTrello size={24} color='primary.main'/>
+    <Box
+      sx={{
+        height: (theme) => theme.trello.appBarHeight,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding:'0 8px'
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <AppsIcon
+          sx={{
+            color: 'primary.main'
+          }}
+        />
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 0.5
+          }}
+        >
+          <FaTrello size={22} color="teal" />
+          <Typography
+            variant="span"
+            sx={{
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              color: 'primary.main'
+            }}
+          >
+            Trello
+          </Typography>
+          <Workspaces />
+          <Recent />
+          <Started />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
       </Box>
-      <Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <TextField
+          id="outlined-search"
+          label="Search..."
+          type="search"
+          size="small"
+        />
         <ModeSelect></ModeSelect>
+        <Tooltip title="Delete">
+          <Badge color="secondary" variant="dot">
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title="Help">
+            <HelpOutlineIcon />
+        </Tooltip>
+        <Profile/>
       </Box>
     </Box>
   )
