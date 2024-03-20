669aeb723286a62f7062bf4ab2b77f9a31263d4e
AnhLeDuc7421 <renyk97@gmail.com>
2024-03-20 10:50:02 +0700

    style app bar

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -8,57 +8,60 @@ import DarkModeIcon from '@mui/icons-material/DarkMode'
 import Brightness4Icon from '@mui/icons-material/Brightness4'
 
 function ModeSelect() {
-    const { mode, setMode } = useColorScheme()
-    const handleChange = (event) => {
-      setMode(event.target.value)
-    }
-  
-    return (
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-        <Select
-          labelId="label-select-dark-light-mode"
-          id="select-dark-light-mode"
-          value={mode}
-          label="Mode"
-          onChange={handleChange}
-        >
-          <MenuItem value={'light'}>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-            >
-              <LightModeIcon />
-              Light
-            </Box>
-          </MenuItem>
-          <MenuItem value={'dark'}>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-            >
-              <DarkModeIcon />
-              Dark
-            </Box>
-          </MenuItem>
-          <MenuItem value={'system'}>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-            >
-              <Brightness4Icon />
-              System
-            </Box>
-          </MenuItem>
-        </Select>
-      </FormControl>
-    )
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
   }
+
+  return (
+    <FormControl sx={{minWidth:121}} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}
+          >
+            <LightModeIcon fontSize='small'/>
+              Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}
+          >
+            <DarkModeIcon fontSize='small'/>
+              Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}
+          >
+            <Brightness4Icon fontSize='small'/>
+              System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 export default ModeSelect
