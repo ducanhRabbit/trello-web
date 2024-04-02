d4f1210226fcdae2b78ab77dd4e1b2be5f6e2d69
ducanhRabbit <renyk97@gmail.com>
2024-04-02 10:24:55 +0700

    dnd-sensor

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,19 +1,48 @@
 import { Box } from '@mui/material'
 import ListColumns from './ListColumns/ListColumns'
 import {mapOrder}  from '~/util/formatter'
+import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({board}) {
-  const oderedColumns = mapOrder(board?.columns,board?.columnOrderIds,'_id')
+  const pointer = useSensor(PointerSensor,{
+    activationConstraint: {
+      distance: 10,
+    },
+  })
+  const sensor = useSensors(pointer)
+  const [oderedColumnsState,setOderedColumnsState] = useState([])
+
+  useEffect(()=>{
+    const oderedColumns = mapOrder(board?.columns,board?.columnOrderIds,'_id')
+    setOderedColumnsState(oderedColumns)
+  },[board])
+
+  const handleDragEnd = (e)=>{
+    console.log(e)
+    const {active,over} = e
+    if(!over) return
+    if(active.id!= over.id){
+      const oldIndex = oderedColumnsState.findIndex((c)=> c._id === active.id)
+      const newIndex = oderedColumnsState.findIndex((c)=> c._id === over.id)
+
+      const dndOderedColumns = arrayMove(oderedColumnsState,oldIndex,newIndex)
+      setOderedColumnsState(dndOderedColumns)
+    }
+  }
   return (
-    <Box sx={{
-      backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      height: (theme) => theme.trello.boardContentHeight,
-      width:'100%',
-      display:'flex',
-      p:'10px 0'
-    }}>
-      <ListColumns columns={oderedColumns}/>
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensor}>
+      <Box sx={{
+        backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        height: (theme) => theme.trello.boardContentHeight,
+        width:'100%',
+        display:'flex',
+        p:'10px 0'
+      }}>
+        <ListColumns columns={oderedColumnsState}/>
+      </Box>
+    </DndContext>
 
   )
 }
